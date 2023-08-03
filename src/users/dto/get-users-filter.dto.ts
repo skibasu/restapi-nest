616be0c4ddd0706abdea7bf99319115ad5547c15
Su <skibasu@gmail.com>
2023-08-03 15:31:03 +0200

    filters validation fixed

--- src/users/dto/get-users-filter.dto.ts
+++ src/users/dto/get-users-filter.dto.ts
@@ -10,15 +10,18 @@ import { UsersRole } from '../types/users-types';
 
 export class GetUsersFilterDto {
   @IsOptional()
+  @IsNotEmpty()
   @IsIn([UsersRole.ADMIN, UsersRole.DRIVER, UsersRole.MANAGER])
   role: UsersRole;
 
   @IsOptional()
+  @IsNotEmpty()
   @IsString()
   @Length(2, 100)
   firstName: string;
 
   @IsOptional()
+  @IsNotEmpty()
   @IsString()
   @Length(2, 100)
   lastName: string;
