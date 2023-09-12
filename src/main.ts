09fa723dc04e6f7d99d11ef18372188e52070941
Su <skibasu@gmail.com>
2023-09-12 16:44:06 +0200

    https conf

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import { AppModule } from './app.module';
 import { AuthGuard } from './auth/quards/auth.guards';
 import { RolesGuard } from './auth/quards/roles.quard';
 import { HttpExceptionFilter } from './filters/http-exceptions.filter';
-import fs from 'fs';
+import * as fs from 'node:fs';
 
 async function bootstrap() {
   const httpsOptions = {
-    key: fs.readFileSync('./secrets/private-key.pem'),
-    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+    key: fs.readFileSync(process.cwd() + '/secret/private-key.pem'),
+    cert: fs.readFileSync(process.cwd() + '/secret/public-certificate.pem'),
   };
   const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors({
